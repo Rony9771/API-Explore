a1a3ce3cb3115b4e33d4769b914de9428e88b127
Rony <rony.ahmed9771@gmail.com>
2022-09-01 00:25:31 +0600

    Country updated

--- js/country.js
+++ js/country.js
@@ -9,19 +9,37 @@ const displayCountry = people =>{
 
   const show = document.getElementById('country-info')
   people.forEach(country => {
-    console.log(country)
+    //console.log(country)
     const countryDiv =document.createElement('div')
     countryDiv.classList.add('country')
     countryDiv.innerHTML =`
     <h2>Country name : ${country.name.official}</h2>
     <p> Capital : ${country.capital ? country.capital[0] : 'no capital'}</p>
+    <button onclick="loadData('${country.cca2}')">Details </button>
     `;
+
     show.appendChild(countryDiv);
-
-    
-  });
-
   
-
+  });
 }
+  const loadData = (code) =>{
+    const url = `https://restcountries.com/v2/alpha/${code}`
+    //console.log('Give Country details', code)
+    //console.log('Give Country details', code)
+    fetch(url)
+    .then (res => res.json())
+    .then (data =>displayDetails(data))
+  }
+
+  const displayDetails =country =>{
+   // console.log(country)
+
+    const showDetails = document.getElementById('country-details')
+    showDetails.innerHTML=`
+    <h2>See Country Flag: ${country.name} </h2>
+    <img src ="${country.flags.png}">
+    `;
+
+  }
+ 
 country();
