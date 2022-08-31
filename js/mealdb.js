9d4f727b452fcfdc522834d537a5550d6250e597
Rony <rony.ahmed9771@gmail.com>
2022-09-01 02:33:11 +0600

    searchbar & Details section added

--- js/mealdb.js
+++ js/mealdb.js
@@ -11,11 +11,11 @@ const displayFood = meals =>{
   const mealDb =document.getElementById('meal-cotainer');
   mealDb.innerHTML = '';
   for (const meal of meals){
-    console.log(meal);
+   // console.log(meal);
     const inserDb =document.createElement('div');
     inserDb.classList.add('col')
     inserDb.innerHTML =`
-    <div class="card">
+    <div onclick="loadFoodDetails(${meal.idMeal})" class="card">
           <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
@@ -32,7 +32,34 @@ const foodSearch =() => {
   foodMeal(searchText);
   seachField.value = '';
 
+}
 
+const loadFoodDetails = (meals) =>{
+  console.log('Food id:', meals);
+  const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meals}`
+  
+  fetch(url)
+  .then (res => res.json())
+  .then (data =>displayFoodDetails(data.meals[0]))
+
+}
+
+const displayFoodDetails = meal =>{
+ console.log(meal)
+  const foodDetail =document.getElementById('meal-details');
+  foodDetail.innerHTML = '';
+  const insertDetails = document.createElement('div')
+  insertDetails.classList.add('card');
+  //const insertDetail = insertDetails.value;
+  insertDetails.innerHTML =`
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${meal.strMeal}</h5>
+          <p class="card-text">${meal.strInstructions}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+  `;
+  foodDetail.appendChild(insertDetails)
 
 }
 //foodMeal('meat');
